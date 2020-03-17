8de6d8087df452f935e40541bf3b82bfff712e04
DineshNavoki <dinesh@navoki.com>
2020-03-17 13:19:37 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,93 +11,74 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _selectedDate;
+  String _selectedTime;
+  String _selectedDateTime;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Flutter Date Time Picker Example"),
       ),
-      body: Container(
+      body:Container(
         width: MediaQuery.of(context).size.width,
-        child:Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(" ${_selectedDate??'Selected Date'}",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
-            RaisedButton(
-              onPressed: () {
-                DatePicker.showDatePicker(context,
-                    showTitleActions: true,
-                    minTime: DateTime(1980, 1, 1),
-                    maxTime: DateTime(3000, 12, 31),
-                    onConfirm: (date) {setState(() {
-                      _selectedDate=DateFormat("yyyy-MM-dd").format(date);
-                    });},
-                    locale: LocaleType.en);},child: Text("Show Date picker"),
-            )
 
+            Text(" ${_selectedDate??"Selected Date"}",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w800),),
+            RaisedButton(onPressed: ()=>_showDatePicker(),child: Text("Show Date Picker"),),
+            Divider(),
+            SizedBox(height: 50,),
+
+            Text(" ${_selectedTime??"Selected Time"}",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w800),),
+            RaisedButton(onPressed: ()=>_showTimePicker(),child: Text("Show Time Picker"),),
+            Divider(),
+
+            SizedBox(height: 50,),
+
+            Text(" ${_selectedDateTime??"Selected Date and Time"}",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w800),),
+            RaisedButton(onPressed: ()=>_showDateTimePicker(),child: Text("Show Date and Time Picker"),),
           ],
-        )
-      )
+        ),
+      ),
     );
   }
+  _showDatePicker()
+  {
+    DatePicker.showDatePicker(context,
+        showTitleActions: true,
+        minTime: DateTime(1980, 1, 1),
+        maxTime: DateTime(3000, 12, 31),
+        onConfirm: (date) {setState(() {
+          _selectedDate=DateFormat("yyyy-MM-dd").format(date);
+        });},
+        locale: LocaleType.en);
+  }
+
+  _showTimePicker()
+  {
+    DatePicker.showTimePicker(context,
+        showTitleActions: true,
+
+        onConfirm: (time) {setState(() {
+          _selectedTime=DateFormat("HH:mm:ss").format(time);
+        });},
+        currentTime: DateTime.now(), locale: LocaleType.en);
 }
-/*       Text("Selected Date is : ${_selecteddate??''}",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
-            RaisedButton(
-              onPressed: () {
-                DatePicker.showDatePicker(context,
-                    showTitleActions: true,
-                    minTime: DateTime(1980, 1, 1),
-                    maxTime: DateTime(3000, 12, 31),
-                onConfirm: (date) {setState(() {
-                  _selecteddate="${date.year}-${date.month}-${date.day}";
-                });},
-                currentTime: DateTime.now(), locale: LocaleType.en);},child: Text("Show Date picker"),
-            )*/
 
-/*     Text("Selected Time is : ${_selecteddate??''}",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
-            RaisedButton(
-              onPressed: () {
-                DatePicker.showTimePicker(context,
-                    showTitleActions: true,
+  _showDateTimePicker()
+{
+  DatePicker.showDateTimePicker(context,
+      showTitleActions: true,
+      minTime: DateTime(1980, 1, 1),
+  maxTime: DateTime(3000, 12, 31),
+onConfirm: (dateTime) {setState(() {
+  print(dateTime);
+  _selectedDateTime=DateFormat("yyyy-MM-dd  HH-mm-ss").format(dateTime);
+});
 
-                    onConfirm: (time) {setState(() {
-                      _selecteddate="${time.hour}:${time.minute}:${time.second}";
-                    });},
-                    currentTime: DateTime.now(), locale: LocaleType.en);},child: Text("Show Date picker"),
-            ),*/
+});
+}
+}
 
-
-
-/*Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(" ${_selectedDate??'Selected Date'}",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
-            RaisedButton(
-              onPressed: () {
-                DatePicker.showDatePicker(context,
-                    showTitleActions: true,
-                    minTime: DateTime(1980, 1, 1),
-                    maxTime: DateTime(3000, 12, 31),
-                    onConfirm: (date) {setState(() {
-                      _selectedDate=DateFormat("yyyy-MM-dd").format(date);
-                    });},
-                    locale: LocaleType.en);},child: Text("Show Date picker"),
-            )
-
-          ],
-        )*/
-/*
-            Text("Selected Date and Time is : ${_selecteddate??''}",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
-            RaisedButton(
-              onPressed: () {
-                DatePicker.showDateTimePicker(context,
-                    showTitleActions: true,
-                    minTime: DateTime(1980, 1, 1),
-                    maxTime: DateTime(3000, 12, 31),
-                    onConfirm: (time) {setState(() {
-                      _selecteddate="${time.hour}:${time.minute}:${time.second}";
-                    });},
-                    currentTime: DateTime.now(), locale: LocaleType.en);},child: Text("Show Date picker"),
-            ),*/
